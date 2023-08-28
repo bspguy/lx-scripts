07289746af86af5e84c71dd0417f2a948b5b3cad
Chuck U. Farley <misteroy@gmail.com>
2023-08-28 14:35:03 +0300

    modifed lx_post_install script

---
+++ lx_post_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# using one liner so you can easily comment out whatever you want
+sudo apt install -y plocate           
+sudo apt install -y cpuinfo
+sudo apt install -y cpu-checker
+sudo apt install -y neofetch
+sudo apt install -y htop
+sudo apt install -y gedit
+sudo apt install -y bashtop
+sudo apt install -y flex
+sudo apt install -y bison
+sudo apt install -y boxes
+sudo apt install -y minicom
+sudo apt install -y dkms
+
+
+# clean up
+sudo apt install -f
+sudo apt autoremove
+sudo apt autoclean
+sudo apt clean
+
+echo "###################################"
+echo "###### lx_post_install DONE! ######"
+echo "###################################"
