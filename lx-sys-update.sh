72f0e0ab4eabed5759be120b17df0e7d98e8a7a1
Roy Cohen <misteroy@gmail.com>
2024-01-05 09:23:53 +0200

    fixed typo

--- lx-sys-update.sh
+++ lx-sys-update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt update -y
+apt list --upgradable #for logging
 sudo apt upgrade --allow-downgrades -y
 sudo apt full-upgrade --allow-downgrades -y
-sudo apt dis-upgrade -y
+sudo apt dist-upgrade -y
