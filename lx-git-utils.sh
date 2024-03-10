681f4402d5af7ec56aa972c3543d243e692b2428
Roy Cohen <misteroy@gmail.com>
2024-03-10 10:38:48 +0200

    Added git diff

--- lx-git-utils.sh
+++ lx-git-utils.sh
@@ -24,6 +24,26 @@ Status()
 	fi
 }
 
+#git diff
+
+#Shows the changes between the working directory and the index. 
+#This shows what has been changed, but is not staged for a commit.
+
+#git diff --cached
+
+#Shows the changes between the index and the HEAD (which is the last commit on this branch). 
+#This shows what has been added to the index and staged for a commit.
+
+#git diff HEAD
+
+#Shows all the changes between the working directory and HEAD (which includes changes in the index). 
+#This shows all the changes since the last commit, whether or not they have been staged for commit or not.
+
+Diff()
+{
+   git diff --cached
+}
+
 Fetch()
 {
 	git fetch
